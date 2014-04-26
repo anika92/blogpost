8c0d4670dfe57c6c1873cec66f9e6ec9c7082fb4
Anika shahabuddin <anikanstu@gmail.com>
2014-04-25 23:56:45 -0700

    last modified

--- logout.php
+++ logout.php
@@ -1,15 +1,8 @@
 ﻿﻿<?php
 
  
-	session_start(); # Starts the session
- 
-	session_unset(); #removes all the variables in the session
- 
-	session_destroy(); #destroys the session
- 
-	if(!$_SESSION[''])
-   		echo "Successfully logged out!<br />";
-	else
-   		 echo "Error Occured!!<br />";
+	session_start();
+session_destroy();
+header('location:index.php');
  
 ?>
