aa6085d3d0a83ed09e759ac5bc8c27a589aacd77
Anika shahabuddin <anikanstu@gmail.com>
2015-03-15 01:37:02 +0600

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'tutorial');
 define('DB_USER','anika_html');
-define('DB_PASSWORD','myfirstweb2323');
+define('DB_PASSWORD','anika');
  
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
